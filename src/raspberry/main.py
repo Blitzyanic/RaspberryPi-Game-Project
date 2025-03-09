a3e05008111d85fb8ab31dd4867aa8e4391abe27
Yanic <yanic.dev@gmail.com>
2025-03-09 12:03:13 +0100

    add sample read for raspberry pi 5

--- src/raspberry/main.py
+++ src/raspberry/main.py
@@ -1,1 +1,13 @@
+import gpiod
 
+# Define the chip and line
+chip = gpiod.Chip('gpiochip0')
+line = chip.get_line(21)
+
+# Request the line as an input
+line.request(consumer='read', type=gpiod.LINE_REQ_DIR_IN)
+
+# Read the value from GPIO pin 21
+value = line.get_value()
+
+print(f'GPIO pin 21 value: {value}')
