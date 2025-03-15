c21737de8183347f234fe95a6a6c3e5f363b711c
Yanic <yanic.dev@gmail.com>
2025-03-15 13:58:38 +0100

    add joystick with y support

--- src/main.py
+++ src/main.py
@@ -163,9 +163,18 @@ class Rpi:
         self.last_button_action = time.time()
         self.button_delay = 0.3  # Delay in seconds between button actions
 
-    def read_joystick(self):
+    def read_joystick_x(self):
         try:
-            self.bus.write_byte(self.address, 0)
+            self.bus.write_byte(self.address, 0)  # Channel 0 for X-axis
+            self.bus.read_byte(self.address)  # Dummy read to start conversion
+            return self.bus.read_byte(self.address)
+        except OSError as e:
+            print(f"I2C communication error: {e}")
+            return None
+
+    def read_joystick_y(self):
+        try:
+            self.bus.write_byte(self.address, 1)  # Channel 1 for Y-axis
             self.bus.read_byte(self.address)  # Dummy read to start conversion
             return self.bus.read_byte(self.address)
         except OSError as e:
@@ -189,13 +198,24 @@ class Rpi:
     def process_joystick(self, game):
         current_time = time.time()
         if current_time - self.last_joystick_action >= self.joystick_delay:
-            joystick_value = self.read_joystick()
-            if joystick_value is not None:
-                if joystick_value < 100:
+            # Read X-axis for left/right movement
+            joystick_x = self.read_joystick_x()
+            if joystick_x is not None:
+                if joystick_x < 100:
                     game.go_side(-1)  # Move left
                     self.last_joystick_action = current_time
-                elif joystick_value > 225:
+                elif joystick_x > 225:
                     game.go_side(1)  # Move right
+                    self.last_joystick_action = current_time
+
+            # Read Y-axis for down movement
+            joystick_y = self.read_joystick_y()
+            if joystick_y is not None:
+                if joystick_y > 225:  # Joystick pushed down
+                    game.go_down()  # Move block down
+                    self.last_joystick_action = current_time
+                elif joystick_y < 100:  # Joystick pulled up
+                    game.rotate()  # Rotate block (alternative to button)
                     self.last_joystick_action = current_time
 
     def reset_game(self):
@@ -259,7 +279,7 @@ while not done:
 
     rpi.process_joystick(game)
 
-    rpi.process_button(game)
+#    rpi.process_button(game)
 
     # Verarbeite Tasteneingaben
     for event in pygame.event.get():
